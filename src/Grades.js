77a4ea2133ea60d777ede1d488a8642a93c04bc7
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-07 21:54:43 -0600

    Created grade list with header

--- src/Grades.js
+++ src/Grades.js
@@ -2,9 +2,54 @@ import React from 'react'
 
 function Grades() {
     return (
-        <div>
-            <h1>Grades Page</h1>
-        </div>
+        <>    
+            <h1 className='grades-header'>Choose Your Grade to Get Started!</h1>
+            <div className='grade-list-container'>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>K</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>1</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>2</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>3</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>4</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>5</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>6</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>7</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>8</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>9</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>10</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>11</div>
+                </div>
+                <div className='grade-item-container'>
+                    <div className='grade-item'>12</div>
+                </div>   
+                <div className='grade-item-container'>
+                    <div className='grade-item'>Extra</div>
+                </div>
+                
+            </div>
+        </>
     )
 }
 
