551160d35776657bbc12124268d22e189ee1e836
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-07 22:16:58 -0600

    Designed layout for Login page

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 function Login() {
     return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <>
+            <div className='login-container'>
+                <h1 className='Login'>Login</h1>
+                <p className='username-text'>UserName</p>
+                <input type='text' id='username-input'></input> 
+                <p className='password-text'>Password</p>
+                <input type='text' id='password-input'></input>
+                <br></br>
+                <input type='submit' value='Submit' id='submit'></input>
+            </div>
+        </>
     )
 }
 
