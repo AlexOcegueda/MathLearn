f8ffb684a3f03b2fe33866d5bc5b2259a92e621a
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-07 22:47:12 -0600

    added website title and adjusted format for banner

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,14 @@ function Home() {
         
         <>
             <div className='begin-banner-container'>
-                <p className='begin-banner-text'>Get Started Right Away Here!</p>
-                <button className='begin-banner-btn'>Begin</button>
+                    
+                <div className='begin-banner-left'>
+                    <h1 className='website-name'>LearnMathToday</h1>
+                </div>
+                    <div className='begin-banner-right'>
+                    <h2 className='begin-banner-text'>Get Started Right Away Here!</h2>
+                    <button className='begin-banner-btn'>Begin</button>
+                </div>
             </div>
             <div className='content-1-home-container'>
                 <h1 className='content-1-home-header'>About Us</h1>
@@ -17,6 +23,9 @@ function Home() {
             <div className='content-2-home-container'>
                 <h1 className='content-2-home-header'>How to Reach Us</h1>
                 <p className='content-2-home-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices neque ornare aenean euismod. Varius sit amet mattis vulputate enim nulla aliquet porttitor. Posuere urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Nulla facilisi etiam dignissim diam quis enim. Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Sapien pellentesque habitant morbi tristique. Gravida dictum fusce ut placerat orci. Purus sit amet luctus venenatis lectus magna fringilla urna porttitor. Elementum eu facilisis sed odio morbi quis commodo odio aenean. Egestas dui id ornare arcu odio. Orci dapibus ultrices in iaculis.</p>
+                <p className='content-2-home-text'> Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Enim sed faucibus turpis in eu mi bibendum neque. Adipiscing diam donec adipiscing tristique. Consequat interdum varius sit amet mattis vulputate enim nulla aliquet. Id volutpat lacus laoreet non. Egestas congue quisque egestas diam in arcu cursus euismod. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Commodo odio aenean sed adipiscing diam donec. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi etiam. Orci ac auctor augue mauris. Volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus.</p>
+                <p className='content-2-home-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices neque ornare aenean euismod. Varius sit amet mattis vulputate enim nulla aliquet porttitor. Posuere urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Nulla facilisi etiam dignissim diam quis enim. Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Sapien pellentesque habitant morbi tristique. Gravida dictum fusce ut placerat orci. Purus sit amet luctus venenatis lectus magna fringilla urna porttitor. Elementum eu facilisis sed odio morbi quis commodo odio aenean. Egestas dui id ornare arcu odio. Orci dapibus ultrices in iaculis.</p>
+                <p className='content-2-home-text'> Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Enim sed faucibus turpis in eu mi bibendum neque. Adipiscing diam donec adipiscing tristique. Consequat interdum varius sit amet mattis vulputate enim nulla aliquet. Id volutpat lacus laoreet non. Egestas congue quisque egestas diam in arcu cursus euismod. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Commodo odio aenean sed adipiscing diam donec. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi etiam. Orci ac auctor augue mauris. Volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus.</p>
             </div>
 
         </>
