de039e9882433000d63b846bb96f22648201aa2a
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-07 21:54:13 -0600

    Removed unnecessary containers to nav

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,18 +3,14 @@ import {Link} from 'react-router-dom'
 
 function NavBar() {
     return (
-        <nav className='nav-container'>
-            <ul className='nav'>
-                <li className='nav-item-container'>
-                    <Link to="/" className='nav-item'>Home</Link>
-                </li>
-                <li className='nav-item-container'>
-                    <Link to="/Grades" className='nav-item'>Grades</Link>
-                </li>
-                <li className='nav-item-container'>
-                    <Link to="/Login" className='nav-item'>Login</Link>
-                </li>
-            </ul>
+        <nav>
+            <div className='nav-container-left'>
+                <Link to="/" className='nav-item'>Home</Link>
+                <Link to="/Grades" className='nav-item'>Grades</Link>
+            </div>
+            <div className='nav-container-right'>
+                <Link to="/Login" className='nav-item'>Login</Link>
+            </div>
         </nav>
     );
 }
