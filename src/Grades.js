f71b7458ce0736b15e11324654b9ca34750562a3
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-08 20:56:43 -0600

    adjusted the look of grade items

--- src/Grades.js
+++ src/Grades.js
@@ -5,47 +5,47 @@ function Grades() {
         <>    
             <h1 className='grades-header'>Choose Your Grade to Get Started!</h1>
             <div className='grade-list-container'>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>K</div>
+                <div className='grade-item-container even'>
+                    <p className='grade-item'>K</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>1st</div>
+                <div className='grade-item-container odd'>
+                    <p className='grade-item'>1st</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>2nd</div>
+                <div className='grade-item-container even'>
+                    <p className='grade-item'>2nd</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>3rd</div>
+                <div className='grade-item-container odd'>
+                    <p className='grade-item'>3rd</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>4th</div>
+                <div className='grade-item-container even'>
+                    <p className='grade-item'>4th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>5th</div>
+                <div className='grade-item-container odd'>
+                    <p className='grade-item'>5th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>6th</div>
+                <div className='grade-item-container even'>
+                    <p className='grade-item'>6th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>7th</div>
+                <div className='grade-item-container odd'>
+                    <p className='grade-item'>7th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>8th</div>
+                <div className='grade-item-container even'>
+                    <p className='grade-item'>8th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>9th</div>
+                <div className='grade-item-container odd'>
+                    <p className='grade-item'>9th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>10th</div>
+                <div className='grade-item-container even'>
+                    <p className='grade-item'>10th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>11th</div>
+                <div className='grade-item-container odd'>
+                    <p className='grade-item'>11th</p>
                 </div>
-                <div className='grade-item-container'>
-                    <div className='grade-item'>12th</div>
+                <div className='grade-item-container even'>
+                    <p className='grade-item'>12th</p>
                 </div>   
-                <div className='grade-item-container'>
-                    <div className='grade-item'>Extra</div>
+                <div className='grade-item-container odd'>
+                    <p className='grade-item'>Extra</p>
                 </div>
             </div>
         </>
