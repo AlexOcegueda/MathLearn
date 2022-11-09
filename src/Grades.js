3b027b316efcb4e3a69222717839a5771d922817
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-08 20:39:44 -0600

    added endings to grade for readability

--- src/Grades.js
+++ src/Grades.js
@@ -9,40 +9,40 @@ function Grades() {
                     <div className='grade-item'>K</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>1</div>
+                    <div className='grade-item'>1st</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>2</div>
+                    <div className='grade-item'>2nd</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>3</div>
+                    <div className='grade-item'>3rd</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>4</div>
+                    <div className='grade-item'>4th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>5</div>
+                    <div className='grade-item'>5th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>6</div>
+                    <div className='grade-item'>6th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>7</div>
+                    <div className='grade-item'>7th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>8</div>
+                    <div className='grade-item'>8th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>9</div>
+                    <div className='grade-item'>9th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>10</div>
+                    <div className='grade-item'>10th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>11</div>
+                    <div className='grade-item'>11th</div>
                 </div>
                 <div className='grade-item-container'>
-                    <div className='grade-item'>12</div>
+                    <div className='grade-item'>12th</div>
                 </div>   
                 <div className='grade-item-container'>
                     <div className='grade-item'>Extra</div>
